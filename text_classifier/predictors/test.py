5633fa882f7228071aa09054c204faddb703d1b7
carlos9310 <52119171+carlos9310@users.noreply.github.com>
2019-08-29 21:11:47 +0800

    Update test.py

--- text_classifier/predictors/test.py
+++ text_classifier/predictors/test.py
@@ -1,10 +1,14 @@
 import json
 import jieba
 from predict import Predictor
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--config_path", help="config path of model")
 
-with open("E:/jiangxinyang/pa_smart_city_nlp/text_classifier/config/mlp_config.json", "r") as fr:
+with open(os.path.join(os.path.abspath(os.path.dirname(os.getcwd())), args.config_path), "r") as fr:
     config = json.load(fr)
+ 
 
 with open("test_data.txt", "r", encoding="utf8") as f:
     data = [line for line in f.readlines()]
